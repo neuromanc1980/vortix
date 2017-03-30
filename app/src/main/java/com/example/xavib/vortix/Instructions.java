76f40a0c08896f19844ac51fcc5de456ee9a2ed9
alexgarcia96 <osamodaerixo@gmail.com>
2017-03-30 16:34:05 +0200

    Clase i layout Instruccions

---
+++ app/src/main/java/com/example/xavib/vortix/Instructions.java
@@ -0,0 +1,44 @@
+package com.example.xavib.vortix;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by alex on 29/3/17.
+ */
+
+public class Instructions {
+
+    protected Button button;
+
+/*
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.instructions);
+
+        Button button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                ImageView iv = (ImageView) findViewById(R.id.);
+                iv.setVisibility(View.VISIBLE);
+
+            }
+
+
+
+
+        });
+
+
+    }*/
+
+
+}
+
